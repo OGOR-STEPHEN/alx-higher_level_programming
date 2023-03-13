fae63f66a3772169df0ae9c11372adcb6787cc4d
OGOR-STEPHEN <ogorstephen485@gmail.com>
2023-03-13 03:10:20 -0700

    task 2

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+from calculator_1.py import calculator
+
+a - 10
+b = 5
+
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
